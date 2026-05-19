8e8d65aa2ceb30fe024c6ee99dffc8ae95ad9490
Angel-Dev <angelv.27.25@gmail.com>
2026-05-19 17:25:49 -0500

    build: add retrofit and lifecycle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,9 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 }
